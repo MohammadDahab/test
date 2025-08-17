18d0a183c6a7183bd0b88627fe4101b7a9b5203f
Mohammed Dahab <88298532+MohammadDahab@users.noreply.github.com>
2025-08-17 12:01:11 +0300

    Update and rename pythonscript to pythonscript.py

---
+++ pythonscript.py
@@ -0,0 +1,14 @@
+
+#EXAMPLE CODE
+
+pip install numpy
+# create array
+import numpy as np
+x=np.array([1,2,3,4,5])
+y=x*4
+y
+height= np.array([1.5,2.5,3.2,4.3,5.1])
+weight= np.array([1,2,3,4,5])
+x= height*weight
+x
+
